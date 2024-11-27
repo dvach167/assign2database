e8b6e0e67ebfe67f618b769f6ce74e026f56d834
Dominik Vachon <150072617+dvach167@users.noreply.github.com>
2024-11-27 14:58:44 -0700

    Update QueryDevelopment.sql
    combined the two querys

--- QueryDevelopment.sql
+++ QueryDevelopment.sql
@@ -7,6 +7,14 @@ ON A.BuildingID = B.BuildingID
 WHERE A.BuildingID = 1
 ORDER BY A.ApartType;
 
+-- 2
+SELECT A.ApartNum, A.ApartType, BD.BldgName, BD.BldgAddr
+    FROM Apartment AS A
+    JOIN Booking BK ON A.ApartID = BK.ApartID
+    WHERE BK.DateStart > "2024-11-21" OR BK.DateEnd < "2024-11-21" # TODO date range
+    JOIN Building BD ON A.BuildingID = BD.BuildingID;
+
+
 -- Requirement 3
 -- List the buildings with the highest number of booked apartments.
 SELECT B.BuildingID, B.Name AS BuildingName, B.Address AS BuildingAddress, COUNT(DISTINCT Bk.ApartID) AS BookedApartments
@@ -31,3 +39,17 @@ FROM Guest AS G
 JOIN Booking AS Bk ON G.GuestID = Bk.GuestID
 GROUP BY G.GuestID, G.FirstName, G.LastName
 HAVING COUNT(Bk.BookID) > 2;
+
+
+-- 6
+SELECT BK.BookID, A.ApartNum, A.ApartType, BD.BldgName, BD.BldgAddr
+    FROM Booking AS BK
+    WHERE BK.DateStart < CURRENT_DATE() AND BK.DateEnd > CURRENT_DATE()
+    JOIN Apartment A ON BK.ApartID = A.ApartID
+    JOIN Building BD ON A.BuildingID = BD.BuildingID;
+
+-- 7
+SELECT A.ApartNum, G.FirstName + " " + G.LastName, "BookingStatus"
+    FROM Booking AS BK
+    WHERE BK.DateStart > "2024-11-21" OR BK.DateEnd < "2024-11-21" # TODO date range
+    JOIN Guest G ON BK.GuestID = G.GuestID;
